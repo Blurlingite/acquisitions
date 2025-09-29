788f140fbdf0635e1ec1d68cebe987664f89f0b3
Blurlingite <vkdawah@gmail.com>
2025-09-29 11:54:04 -0400

    Implement CI/CD pipelines and GitHub Actions

--- src/app.js
+++ src/app.js
@@ -38,10 +38,13 @@ app.get("/health", (req, res) => {
 });
 
 app.get("/api", (req, res) => {
-  res.status(200).send("Acquisitions API is now running!");
+  res.status(200).json({ message: "Acquisitions API is running!" });
 });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
 export default app;
